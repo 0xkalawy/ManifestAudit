67070b75951ae5dd236f9ddd378b69c7eadd73ca
Wego <wagdym014@gmail.com>
2024-08-08 00:17:43 +0300

    Middle

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 # print colored text
 def print_colored(text, color):
-    if text!=None:
-        print(f"{color}{text}")
-    else:
-        print(f"{ENDC}")
+    print(f"{color}{text}",end="")
+    print(f"{ENDC}")
 
 # get the AndroidManifest nemspace specified in the root element
 def get_namespace(file):
@@ -26,58 +24,71 @@ def get_namespace(file):
     
 # print AndroidManifest.xml
 def get_activities(root:ET.Element,android_namespace:str):
-    activities = []
     exported_activities = []
+    print_colored("All activities:",BLUE)
     for activity in root.findall(".//activity"):
         name = activity.get(f"{{{android_namespace}}}name")
-        activities.append(name)
+        print_colored(name,ENDC)
         exported = activity.get(f"{{{android_namespace}}}exported")
         if exported == 'true':
             exported_activities.append(name)
-    print_colored("All activities:",BLUE)
-    for activity in activities:
-        print_colored(activity,ENDC)
     print("")
     print_colored("Activity with exported flag",RED)
     for activity in exported_activities:
         print_colored(activity,ENDC)
- 
+    
+        
+# print custom permissions 
 def get_custom_permissions(root):
-    print_colored("Custom Permissions:",YELLOW)
+    print_colored("Custom Permissions:",BLUE)
     for permission in root.findall(".//permission"):
         name = permission.get(f"{{{android_namespace}}}name")
         print_colored(name,ENDC)
        
+# print uses permission
+def get_uses_permissions(root:ET.Element):
+    permissions = root.findall(".//uses-permission")
+    print_colored("Uses Permissions:",BLUE)
+    
+    for permission in permissions:
+        print(permission.get(f"{{{android_namespace}}}name"))
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AndroidManifest.xml file analyzing and cutting down")
     parser.add_argument('-f', '--file', required=True, help='Path to the AndroidManifest.xml file')
     parser.add_argument('-o', '--output', help='Output file (Only text file is supported)')
     parser.add_argument('--activities', action='store_true', help='Print only activities')
     parser.add_argument('--get-namespace', action='store_true', help='Print only namespace')
-    parser.add_argument('--namespace', action='store_true', help='provide the namespace of the file (if not set the tool will determine it by itself)')
+    # parser.add_argument('--namespace', action='store_true', help='provide the namespace of the file (if not set the tool will determine it by itself)')
     parser.add_argument('--custom-permissions', action='store_true', help='provide only the permissions defined by the developer')
+    parser.add_argument('--uses-permissions', action='store_true', help='provide needed permission for the application to run')
     parser.add_argument('--dump', action='store_true', help='dump the whole file and extract all possible data')
-    
     args = parser.parse_args()
     
-    android_namespace = get_namespace(args.file)
+    android_namespace = get_namespace(args.file) if not args.namespace else args.namespace
     root = ET.parse(args.file).getroot()
+    print(f"Working with the namespace ",end="")
+    print_colored(android_namespace,CYAN)
+    print()
+
     if args.dump:
-        print(f"Working with the namespace ",end="")
-        print_colored(android_namespace,CYAN)
-        print_colored(None,ENDC)
         get_activities(root,android_namespace)
-        print_colored(None,ENDC)
+        print()
         get_custom_permissions(root)
+        print()
+        get_uses_permissions(root)
+        
 
     elif args.get_namespace:
-        print(get_namespace(args.file))
-        print_colored(None,ENDC)
+        exit()        
 
     elif args.activities:
         get_activities(root,android_namespace)
-        print_colored(None,ENDC)
+        
         
     elif args.custom_permissions:
         get_custom_permissions(root)
-        print_colored(None,ENDC)
+        
+    elif args.uses_permissions:
+        get_uses_permissions(root)
+        
